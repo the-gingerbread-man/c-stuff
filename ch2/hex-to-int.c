2e6767c29dc599320e31f9c94a4851e312e8b983
the-gingerbread-man <alex.patch3@gmail.com>
2017-03-25 00:58:14 -0500

    Works for all values, changed types to long int

--- ch2/hex-to-int.c
+++ ch2/hex-to-int.c
@@ -1,49 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-int htoi(char digits[], int length, int base);
+long htoi(char digits[], int length);
 
 int main(void)
 {
-  char test[10] = "fffffff";
+  char test[12] = "ffffffffffff";
 
-  printf("%d", htoi(test, 7, 16));
+  printf("%ld", htoi(test, 12));
+  return 0;
 }
 
-/* htoi: converts a string of hex digits to a long integer
-    assumes BIG ENDIAN */
-int htoi(char digits[], int length, int base)
+/**
+  * htoi: Converts a string of hex digits to a long integer
+  *       Note: assumes BIG ENDIAN inputs
+  */
+long htoi(char digits[], int length)
 {
   char digit;
-  int dec, exp, place, placevalue;
+  int exp;
+  long dec, place;
 
   dec = exp = 0;
 
   while (length > 0) {
-    place = pow(base, exp++);
+    place = pow(16, exp++);
     digit = digits[--length];
 
-    printf("Dec:\t\t\t%d\n", dec);
-    printf("Place:\t\t\t%d\n", place);
-    printf("ASCII Digit:\t\t%d\t%c\n", digits[length], digit);
-
-    if ('0' <= digit && digit <= '9') {
+    if ('0' <= digit && digit <= '9')
       dec += (digit - '0') * place;
-      printf("0-9 Digit Value:\t%d\n", (digit - '0'));
-      printf("Added:\t%d\n", (digit - '0') * place);
-    }
-    else if ('A' <= digit && digit <= 'F') {
+    else if ('A' <= digit && digit <= 'F')
       dec += ((digit - 'A') + 10) * place;
-      printf("A-Z Digit Value:\t%d\n", (digit - 'A') + 10);
-      printf("Added:\t%d\n", ((digit - 'A') + 10) * place);
-    }
-    else if ('a' <= digit && digit <= 'f') {
+    else if ('a' <= digit && digit <= 'f')
       dec += ((digit - 'a') + 10) * place;
-      printf("a-z Digit Value:\t%d\n", (digit - 'a') + 10);
-      printf("Added:\t%d\n", ((digit - 'a') + 10) * place);
-    }
-
-    printf("----------\n");
   }
 
   return dec;
