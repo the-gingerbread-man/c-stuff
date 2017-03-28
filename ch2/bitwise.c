d3082bb9ed07a185304814736927eceee8239b12
the-gingerbread-man <alex.patch3@gmail.com>
2017-03-28 03:15:55 -0500

    added bettercreatebitwise, using left shifts instead of the pow() function

--- ch2/bitwise.c
+++ ch2/bitwise.c
@@ -2,15 +2,23 @@
 #import <math.h>
 
 unsigned int createbitmask32(int n, int p);
+unsigned int bettercreatebitmask32(int n, int p);
 
 int main(void)
 {
   unsigned int test;
 
-  test = createbitmask32(1, 30); // -> 0x4
-  test = createbitmask32(1, 0);  // -> 0x80000000
-  test = createbitmask32(1, 4);  // -> 0x8000000
-  test = createbitmask32(3, 8);  // -> 0xe00000
+  // test = createbitmask32(1, 30); // -> 0x4
+  // test = createbitmask32(1, 0);  // -> 0x80000000
+  // test = createbitmask32(1, 4);  // -> 0x8000000
+  // test = createbitmask32(3, 8);  // -> 0xe00000
+  // test = createbitmask32(1, 31); // -> 0x1
+
+  // test = bettercreatebitmask32(1, 30); // -> 0x4
+  // test = bettercreatebitmask32(1, 0);  // -> 0x80000000
+  // test = bettercreatebitmask32(1, 4);  // -> 0x8000000
+  // test = bettercreatebitmask32(3, 8);  // -> 0xe00000
+  // test = bettercreatebitmask32(1, 31); // -> 0x1
 
   return 0;
 }
@@ -27,6 +35,20 @@ unsigned int createbitmask32(int n, int p)
     placevalue = (unsigned) pow(2, exponent--);
     mask = mask | placevalue;
   }
+
+  return mask;
+}
+
+/* bettercreatebitmask32: creates a bitmask using left shifts, rather than the power function */
+unsigned int bettercreatebitmask32(int n, int p)
+{
+  unsigned int mask;
+
+  mask = 0;
+  p = 31 - p;
+
+  while (n-- > 0)
+    mask = mask | 1 << p--;
 
   return mask;
 }
