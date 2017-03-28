8e9f354d4b683e01367b0df805cf4e56f44d906b
the-gingerbread-man <alex.patch3@gmail.com>
2017-03-28 05:23:07 -0500

    Improve createbitmask32() and created invert()

--- ch2/bitwise.c
+++ ch2/bitwise.c
@@ -1,34 +1,35 @@
 #import <stdio.h>
 #import <math.h>
 
+unsigned int cruddybitmask32(int n, int p);
 unsigned int createbitmask32(int n, int p);
-unsigned int bettercreatebitmask32(int n, int p);
-unsigned int setbits(unsigned int x, int p, int n, unsigned int y);
+unsigned int setbits(unsigned int x, int n, int p, unsigned int y);
+unsigned int invert(unsigned int x, int n, int p);
 
 int main(void)
 {
   unsigned int test;
+
+  // test = cruddybitmask32(1, 30); // -> 0x4
+  // test = cruddybitmask32(1, 0);  // -> 0x80000000
+  // test = cruddybitmask32(1, 4);  // -> 0x8000000
+  // test = cruddybitmask32(3, 8);  // -> 0xe00000
+  // test = cruddybitmask32(1, 31); // -> 0x1
 
   // test = createbitmask32(1, 30); // -> 0x4
   // test = createbitmask32(1, 0);  // -> 0x80000000
   // test = createbitmask32(1, 4);  // -> 0x8000000
   // test = createbitmask32(3, 8);  // -> 0xe00000
   // test = createbitmask32(1, 31); // -> 0x1
-
-  // test = bettercreatebitmask32(1, 30); // -> 0x4
-  // test = bettercreatebitmask32(1, 0);  // -> 0x80000000
-  // test = bettercreatebitmask32(1, 4);  // -> 0x8000000
-  // test = bettercreatebitmask32(3, 8);  // -> 0xe00000
-  // test = bettercreatebitmask32(1, 31); // -> 0x1
-  // test = bettercreatebitmask32(2, 2); // -> 0x30000000
+  // test = createbitmask32(2, 2); // -> 0x30000000
 
   test = setbits(0xF0, 3, 27, 0x55); // -> 0xf4
 
   return 0;
 }
 
-/* createbitmask32: creates a 32-bit mask with the n bits starting at bit (32 - p) set to 1 */
-unsigned int createbitmask32(int n, int p)
+/* cruddybitmask32: creates a 32-bit mask with the n bits starting at bit (32 - p) set to 1 */
+unsigned int cruddybitmask32(int n, int p)
 {
   unsigned int mask, exponent, placevalue;
 
@@ -43,25 +44,26 @@ unsigned int createbitmask32(int n, int p)
   return mask;
 }
 
-/* bettercreatebitmask32: creates a bitmask using left shifts, rather than the power function */
-unsigned int bettercreatebitmask32(int n, int p)
+/* reatebitmask32: creates a bitmask using left shifts, rather than the power function */
+unsigned int createbitmask32(int n, int p)
 {
-  unsigned int mask;
+  return ~(~0U << n) << p;
+  // unsigned int mask;
 
-  mask = 0;
-  p = 31 - p;
+  // mask = 0;
+  // p = 31 - p;
 
-  while (n-- > 0)
-    mask = mask | 1 << p--;
+  // while (n-- > 0)
+  //   mask = mask | 1 << p--;
 
-  return mask;
+  // return mask;
 }
 
 /* setbits: set the n bits beginning at position p (big to small) to the rightmost n bits of y */
 unsigned int setbits(unsigned int x, int n, int p, unsigned int y)
 {
   unsigned int mask;
-  mask = bettercreatebitmask32(n, p);
+  mask = createbitmask32(n, p);
 
   y = y << (32 - n - p);
   y = y & mask;
@@ -69,4 +71,11 @@ unsigned int setbits(unsigned int x, int n, int p, unsigned int y)
 
   return x | y;
 
+}
+
+/* invert: inverts the n bits beginning at position p */
+unsigned int invert(unsigned int x, int n, int p)
+{
+  // cheating: return x ^ bettercreatebitmask32(n, p);
+  return x ^ (~(~0U << n) << p);
 }
