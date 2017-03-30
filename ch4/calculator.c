8bf5066d92b0b5dcda6c8c3dccf54c011d756015
the-gingerbread-man <alex.patch3@gmail.com>
2017-03-30 10:24:14 -0500

    cleaned up calculator.c

--- ch4/calculator.c
+++ ch4/calculator.c
@@ -1,10 +1,10 @@
 #include <ctype.h>
 #include <stdio.h>
-#define MAXLINE 40
+#define MAXOP 100 /* maximum number of args for an operator */
 
 int calculator();
-char push(char a, char stack[]);
-char pop(char stack[]);
+char push(char, char[]);
+char pop(char[]);
 void teststack(void);
 
 int main(void)
@@ -15,25 +15,16 @@ int main(void)
 /* calculator: returns the value of a sequence of reverse-polish ordered values and operations */
 int calculator()
 {
-  char stack[MAXLINE];
-  stack[0] = MAXLINE - 1; /* stack size (- 1 for the np) */
+  char stack[MAXOP];
+  stack[0] = MAXOP - 1; /* stack size (- 1 for the np) */
   stack[1] = 0; /* top of the stack */
 
   int i;
-  for (i = 2; i < (MAXLINE - 2); ++i)
+  for (i = 2; i < (MAXOP - 2); ++i)
     stack[i] = 0;
 
-  char c;
+  char c, op2;
 
-  // printf("Top:\t%c\n", stack[stack[1] + 1]);
-  // printf("T:\t%d\n", stack[1]);
-  // push(1, stack);
-  // push(2, stack);
-  // push(3, stack);
-  // printf("Pop:\t%d\n", pop(stack));
-  // printf("Pop:\t%d\n", pop(stack));
-  // printf("T:\t%d\n", stack[1] + 2);
-  // printf("Top:\t%c\n", stack[stack[1] + 1]);
   while ((c = getchar()) != EOF) {
 
     if (isspace(c) && c != '\n')
@@ -49,22 +40,31 @@ int calculator()
         push(pop(stack) + pop(stack), stack);
         break;
       case '-':
-        push(pop(stack) - pop(stack), stack);
+        op2 = pop(stack);
+        push(pop(stack) - op2, stack);
         break;
       case '*':
         push(pop(stack) * pop(stack), stack);
         break;
       case '/':
-        push(pop(stack) / pop(stack), stack);
+        op2 = pop(stack);
+        if (op2 != 0)
+          push(pop(stack) / op2, stack);
+        else
+          printf("error: zero divisor\n");
         break;
       case '%':
-        push(pop(stack) % pop(stack), stack);
+        op2 = pop(stack);
+        if (op2 != 0)
+          push(pop(stack) % op2, stack);
+        else
+          printf("error: zero divisor\n");
         break;
       case '\n':
         printf("Out: %d\n", pop(stack));
         break;
       default:
-        printf("Unrecogrized character: %c\n", c);
+        printf("error: unrecogrized character: %c\n", c);
         break; /* error */
     }
   }
@@ -76,8 +76,10 @@ int calculator()
 /* push: pushes a onto the top of a stack, returning the number of elements in the stack */
 char push(char a, char stack[])
 {
-  if (stack[1] + 1 >= stack[0]) /* Error: stack overflow */
-    return '\0';
+  if (stack[1] + 1 >= stack[0]) {
+    printf("error: stack full, can't push %c\n", a);
+    return 0;
+  }
 
   stack[stack[1] + 2] = a; /* +2 because the first two elements are metadata */
   return ++stack[1];
@@ -86,34 +88,10 @@ char push(char a, char stack[])
 /* pop: removes the topmost element of a stack, and returns that element */
 char pop(char stack[])
 {
-  if (stack[1] < 1) /* Error: stack is empty */
-    return '\0';
+  if (stack[1] < 1) {
+    printf("error: stack already empty, can't pop\n");
+    return 0;
+  }
 
   return stack[--stack[1] + 2];
-}
-
-/* teststack: perform tests on the push and pop functions */
-void teststack(void)
-{
-  char ts[13];
-  ts[0] = 12; /* size, not including null terminator */
-  ts[1] = 0; /* top */
-
-  int i;
-  for (i = 2; i < 12; ++i)
-    ts[i] = 0;
-
-  push('a', ts);
-  push('b', ts);
-  push('c', ts);
-  push('d', ts);
-  push('f', ts);
-  pop(ts);
-  pop(ts);
-  push('x', ts);
-  push('y', ts);
-  pop(ts);
-  push('z', ts);
-
-  printf("ts:\t%s\n", ts); // -> "  abcxz"
 }
