ac6c19b2e43372e6507e46fde2729b4874b8e539
the-gingerbread-man <alex.patch3@gmail.com>
2017-03-30 11:18:47 -0500

    refactored calculator to use icky shared mutables

--- ch4/calculator.c
+++ ch4/calculator.c
@@ -1,97 +1,133 @@
 #include <ctype.h>
 #include <stdio.h>
-#define MAXOP 100 /* maximum number of args for an operator */
+#include <stdlib.h>
 
-int calculator();
-char push(char, char[]);
-char pop(char[]);
-void teststack(void);
+#define BUFSIZE 100
+#define MAXOP 100 /* maximum number of args for an operator */
+#define MAXVAL 100
+#define NUMBER '0'
+
+int getop(char []);
+
+char getch(void);
+void ungetch(char);
+
+void push(double);
+double pop();
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+double val[MAXVAL];
+int sp = 0; /* next free stack position */
 
 int main(void)
 {
-  calculator();
-}
+  int type;
+  double op2;
+  char s[MAXOP];
 
-/* calculator: returns the value of a sequence of reverse-polish ordered values and operations */
-int calculator()
-{
-  char stack[MAXOP];
-  stack[0] = MAXOP - 1; /* stack size (- 1 for the np) */
-  stack[1] = 0; /* top of the stack */
-
-  int i;
-  for (i = 2; i < (MAXOP - 2); ++i)
-    stack[i] = 0;
-
-  char c, op2;
-
-  while ((c = getchar()) != EOF) {
-
-    if (isspace(c) && c != '\n')
-      continue;
-
-    if (c <= '9' && c >= '0') {
-      push(c - '0', stack);
-      continue;
-    }
-
-    switch (c) {
+  while ((type = getop(s)) != EOF) {
+    switch(type) {
+      case NUMBER:
+        push(atof(s));
+        break;
       case '+':
-        push(pop(stack) + pop(stack), stack);
+        push(pop() + pop());
         break;
       case '-':
-        op2 = pop(stack);
-        push(pop(stack) - op2, stack);
+        op2 = pop();
+        push(pop() - op2);
         break;
       case '*':
-        push(pop(stack) * pop(stack), stack);
+        push(pop() * pop());
         break;
       case '/':
-        op2 = pop(stack);
-        if (op2 != 0)
-          push(pop(stack) / op2, stack);
+        op2 = pop();
+        if (op2 != 0.0)
+          push(op2 / pop());
         else
           printf("error: zero divisor\n");
         break;
       case '%':
-        op2 = pop(stack);
-        if (op2 != 0)
-          push(pop(stack) % op2, stack);
+        op2 = pop();
+        if (op2 != 0.0)
+          push((int) op2 % (int) pop());
         else
           printf("error: zero divisor\n");
         break;
       case '\n':
-        printf("Out: %d\n", pop(stack));
+        printf("\t%.8g\n", pop());
         break;
       default:
-        printf("error: unrecogrized character: %c\n", c);
-        break; /* error */
+        printf("error: unknown command %s\n", s);
+        break;
+      }
     }
-  }
-
-  return 0; /* what should the return be? the calculated values should have all been output with printf() */
-
+    return 0;
 }
 
-/* push: pushes a onto the top of a stack, returning the number of elements in the stack */
-char push(char a, char stack[])
+/* getch: gets the next character from the buffer, or, if it is empty, the next character from stdin */
+char getch(void)
 {
-  if (stack[1] + 1 >= stack[0]) {
-    printf("error: stack full, can't push %c\n", a);
-    return 0;
-  }
-
-  stack[stack[1] + 2] = a; /* +2 because the first two elements are metadata */
-  return ++stack[1];
+  return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
-/* pop: removes the topmost element of a stack, and returns that element */
-char pop(char stack[])
+/* ungetch: push character back into input buffer */
+void ungetch(char c)
 {
-  if (stack[1] < 1) {
-    printf("error: stack already empty, can't pop\n");
-    return 0;
-  }
+  if (bufp >= BUFSIZE)
+    printf("error: ungetch input buffer overflow\n");
+  else
+    buf[bufp++] = c;
+}
 
-  return stack[--stack[1] + 2];
+/* getop: gets the next input character or numeric operand */
+int getop(char s[])
+{
+  int i, c;
+
+  while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ; /* ignore spaces and tabs */
+
+  s[1] = '\0';
+
+  if (!isdigit(c) && c != '.')
+    return c; /* not a number */
+  i = 0;
+
+  if (isdigit(c)) /* collect integer part */
+    while (isdigit(s[++i] = c = getch()))
+      ;
+
+  if (c == '.') /* collect fraction part */
+      while (isdigit(s[++i] = c = getch()))
+        ;
+
+  s[i] = '\0';
+
+  if (c != EOF)
+    ungetch(c);
+
+  return NUMBER;
+}
+
+/* push: pushes a onto the top of the stack */
+void push(double a)
+{
+  if (sp < MAXVAL)
+    val[sp++] = a;
+  else
+    printf("error: stack full, can't push %f\n", a);
+}
+
+/* pop: removes the topmost element of the stack, and returns that element */
+double pop(void)
+{
+  if (sp > 0)
+    return val[--sp];
+  else {
+    printf("error: stack empty, can't pop\n");
+    return 0.0;
+  }
 }
