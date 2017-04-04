3830b22f9cb72b110d4371672ead3de785f75b79
the-gingerbread-man <alex.patch3@gmail.com>
2017-04-04 00:56:27 -0500

    improved calculator to handle exponents and negative numbers

--- ch4/calculator.c
+++ ch4/calculator.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFSIZE 100
+#define BUFSIZE 100 /* maximum size of the input buffer */
 #define MAXOP 100 /* maximum number of args for an operator */
-#define MAXVAL 100
-#define NUMBER '0'
+#define MAXVAL 100 /* total size of the calculator stack */
+#define NUMBER '0' /* type identifier for getop() and main() */
 
 int getop(char []);
 
@@ -15,22 +15,23 @@ void ungetch(char);
 void push(double);
 double pop();
 
-char buf[BUFSIZE];
-int bufp = 0;
+char buf[BUFSIZE]; /* input buffer stack */
+int bufp = 0; /* next free stack position */
 
-double val[MAXVAL];
+double val[MAXVAL]; /* calculator stack */
 int sp = 0; /* next free stack position */
 
+/* reverse polish calculator */
 int main(void)
 {
   int type;
-  double op2;
-  char s[MAXOP];
+  double op2; /* used to store 2nd operand for non-commutative operators */
+  char s[MAXOP]; /* raw characters (digits) of a single input value */
 
   while ((type = getop(s)) != EOF) {
     switch(type) {
       case NUMBER:
-        push(atof(s));
+        push(atof(s)); /* the calculator operates on doubles */
         break;
       case '+':
         push(pop() + pop());
@@ -52,7 +53,7 @@ int main(void)
       case '%':
         op2 = pop();
         if (op2 != 0.0)
-          push((int) op2 % (int) pop());
+          push((double) ((long) op2 % (long) pop()));
         else
           printf("error: zero divisor\n");
         break;
@@ -82,37 +83,55 @@ void ungetch(char c)
     buf[bufp++] = c;
 }
 
-/* getop: gets the next input character or numeric operand */
+/* getop: reads the next character or numeric operand into s[]
+    and returns it, or NUMBER if it is a number */
 int getop(char s[])
 {
   int i, c;
 
-  while ((s[0] = c = getch()) == ' ' || c == '\t')
-    ; /* ignore spaces and tabs */
+  // while ((s[0] = c = getch()) == ' ' || c == '\t')
+  //   ; /* ignore blanks and tabs */
+
+  do { /* ignore blanks and tabs */
+    s[0] = c = getch();
+  } while (c == ' ' || c == '\t');
 
   s[1] = '\0';
 
-  if (!isdigit(c) && c != '.')
-    return c; /* not a number */
-  i = 0;
+  if (!isdigit(c) && c != '.' && c != '-' && c != 'e' && c != 'E')
+    return c; /* not a number or part of a number */
 
-  if (isdigit(c)) /* collect integer part */
+  i = 0;
+  if (c == '-')
+    if (isdigit(c = getch())) /* negative number */
+      s[i++] = c;
+    else { /* minus operator */
+      ungetch(c);
+      return '-';
+    }
+
+  if (isdigit(c)) /* collect integer part (plus the character the loop fails on, most likely ' ') */
     while (isdigit(s[++i] = c = getch()))
       ;
 
   if (c == '.') /* collect fraction part */
-      while (isdigit(s[++i] = c = getch()))
-        ;
+    while (isdigit(s[++i] = c = getch()))
+      ;
+
+  if (c == 'e' || c == 'E') /* collect exponent */
+    while (isdigit(s[++i] = c = getch()))
+      ;
 
   s[i] = '\0';
 
   if (c != EOF)
-    ungetch(c);
+    ungetch(c); /* the character the while condition failed on */
 
+  printf("%s\t%f\n", s, atof(s)); /* for debugging */
   return NUMBER;
 }
 
-/* push: pushes a onto the top of the stack */
+/* push: pushes a onto the top of the calculator stack val[] */
 void push(double a)
 {
   if (sp < MAXVAL)
@@ -121,7 +140,7 @@ void push(double a)
     printf("error: stack full, can't push %f\n", a);
 }
 
-/* pop: removes the topmost element of the stack, and returns that element */
+/* pop: removes the topmost element of the calculator stack val[], and returns that element */
 double pop(void)
 {
   if (sp > 0)
